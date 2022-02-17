bcb641b827d357c76f991bfb04a77731e172ddb3
Kamogelo Joy Letsoalo <joykamogelo@gmail.com>
2022-02-17 23:25:47 +0200

    added Palindrome checker files

---
+++ scripts/main.js
@@ -0,0 +1,21 @@
+
+let outputText = document.getElementById("checkerOutput");
+let wordToCheck = document.getElementById("checkWord");
+
+
+function isPalindrome(){
+    const reverser  = str => str.split("").reverse().join("");
+    //const phraseReverser  = str => str.split("").reverse().join("").split(" ").join("");  will be using the commented out code to check is phrases are palindromes
+    let reverseWordNumberToCheck = reverser(wordToCheck.value);
+    //let reversePhraseToCheck = phraseReverser(wordToCheck.value);
+    //let reversePhrase = wordToCheck.value.split(" ");
+
+    if(reverseWordNumberToCheck !== "" && reverseWordNumberToCheck !== " "){
+        wordToCheck.value == reverseWordNumberToCheck ? outputText.innerText = wordToCheck.value +" " + "is a palindrome" : 
+        outputText.innerText = wordToCheck.value +" " + "is not a palindrome";    
+    }
+    else{
+        alert("Please enter a word/number to check whether it is a palindrome or not");
+    }
+    
+}
